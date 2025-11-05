21f9b516df9e5792a0a447c00392bfc1b201a806
marsyadindaaa <adindamarsya33@gmail.com>
2025-11-06 01:15:18 +0700

    Add files via upload

---
+++ Pertemuan 2/Praktikum/P2_1_714240039/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714240039
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===PERCOBAAN SINTAKS INPUT===");
+
+            Console.Write("angka pertama: ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.Write("angka kedua: ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{input} + {input2}= {input + input2}");
+            Console.WriteLine($"{input} - {input2}= {input - input2}");
+            Console.WriteLine($"{input} x {input2}= {input * input2}");
+            Console.WriteLine($"{input} : {input2}= {input / input2}");
+
+
+
+
+
+        }
+    }
+}
