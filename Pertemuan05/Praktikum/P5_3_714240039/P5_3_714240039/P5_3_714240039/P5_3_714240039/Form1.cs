b737f9bb02a3f1f1b11598c583a33055a9e0e28d
marsyadindaaa <adindamarsya33@gmail.com>
2025-11-11 00:02:41 +0700

    Add files via upload

---
+++ Pertemuan05/Praktikum/P5_3_714240039/P5_3_714240039/P5_3_714240039/P5_3_714240039/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_714240039
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            string os = "";
+            string status = "";
+
+            if (rb_android.Checked == true)
+            {
+                os = "Android";
+            }
+            else if (rb_ios.Checked == true)
+            {
+                os = "iOS";
+            }
+
+            if (cbYa.Checked == true)
+            {
+                status = "Ya, sudah diperbaiki";
+            }
+            else if (cbYa.Checked == false)
+            {
+                status = "Belum diperbaiki";
+            }
+
+            MessageBox.Show(
+                "Merk HP : " + txtMerkHP.Text +
+                "\nSistem Operasi : " + os +
+                "\nStatus Perbaikan : " + status,
+                "Informasi Service HP",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
