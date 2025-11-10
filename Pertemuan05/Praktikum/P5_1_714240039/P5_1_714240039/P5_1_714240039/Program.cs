a4367ce6aceec8945747b571dece8ad24f15d1d4
marsyadindaaa <adindamarsya33@gmail.com>
2025-11-11 00:00:54 +0700

    Add files via upload

---
+++ Pertemuan05/Praktikum/P5_1_714240039/P5_1_714240039/P5_1_714240039/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_1_7142400359
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new P5_1_714240039.Form1()); // ← inilah yang menampilkan form
+        }
+    }
+}
